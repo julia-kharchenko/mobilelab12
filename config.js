528ea38ef10f7070ef27e512dc2affdeaeca5348
Julia Kharchenko <123120841+julia-kharchenko@users.noreply.github.com>
2024-04-22 14:43:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const Config = {
-    newsUrl: ' ',
-    galleryUrl: ' ',
+    newsUrl: 'https://raw.githubusercontent.com/julia-kharchenko/mobilelab12/main/data/news.json?token=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',
+    galleryUrl: 'https://raw.githubusercontent.com/julia-kharchenko/mobilelab12/main/data/images.json?token=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',
 };
 
 export default Config;
